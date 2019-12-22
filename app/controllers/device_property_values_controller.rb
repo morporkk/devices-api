4412336e6bd4226e20ef00edad7010a2d14680c1
Miroslav <miroslavr@protonmail.com>
2019-12-22 22:48:24 +0100

    Make device_property_values controller
    For now it will just have #index and #show actions as others will
    probably handled by device/device_type controller
    
    - Add index and show routes

---
+++ app/controllers/device_property_values_controller.rb
@@ -0,0 +1,11 @@
+class DevicePropertyValuesController < ApplicationController
+  def index
+    @device_property_values = DevicePropertyValue.all
+    render json: @device_property_values
+  end
+
+  def show
+    @device_property_value = DevicePropertyValue.find(params[:id])
+    render json: @device_property_value
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :device_types
   resources :devices
   resources :device_type_properties, only: [:index]
+  resources :device_property_values, only: %i[index show]
 end
