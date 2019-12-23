0b93fbba0f24b9e5c73871427cf750c217420d26
Miroslav <miroslavr@protonmail.com>
2019-12-23 20:34:05 +0100

    Fix type validation in DevicePropertyValue
    Grabs DeviceType of given device, use coerce_tree gem function to get
    all parents, and check if it matches given property.
    
    Right now it's taking 3 querys to acomplish this so I'll try to improve
    it.
    
    Not tested because coreced_tree is not optimizaed for minitest, will try
    to find workaround.

--- app/models/device_property_value.rb
+++ app/models/device_property_value.rb
@@ -3,14 +3,19 @@ class DevicePropertyValue < ApplicationRecord
   belongs_to :device_type_property
 
   validates :value, presence: true, length: { maximum: 65 }
-  validate  :same_type?
+  validate  :correct_type
 
   private
 
-  # Temporary, doesn't check parent types right noww
-  # Have to make hierachical traversing up in device_type model first
-  def same_type?
-    return if device.device_type_id == device_type_property.device_type_id
+  # Traverse up DeviceType tree to get all parent id's and match if property
+  # belongs to any type in path.
+  # Only tested manualy 
+  # TODO: - Try to optimize
+  #       - Try again to integrate coerce gem into minitest
+  def correct_type
+    type = DeviceType.where('id = ?', device.device_type_id).take
+    path = type.self_and_ancestors_ids
+    return if path.include?(device_type_property.device_type_id)
 
     errors.add(:device_type_property_id, "must belong to same type")
   end
