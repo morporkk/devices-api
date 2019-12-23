a1034c16a8142a79dfb54359c4fbba323d58e603
Miroslav <miroslavr@protonmail.com>
2019-12-23 21:54:02 +0100

    Add DeviceType#properties

--- app/models/device_type.rb
+++ app/models/device_type.rb
@@ -5,4 +5,9 @@ class DeviceType < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 65 },
                    uniqueness: true
+
+  # Return all properties of given DeviceType and it's ancestors in tree
+  def properties
+    DeviceTypeProperty.where(device_type_id: self_and_ancestors_ids)
+  end
 end
