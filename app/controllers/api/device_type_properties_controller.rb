f1ad9281e172af011f8c0ca05f8c0c73229f21ec
Miroslav <miroslavr@protonmail.com>
2019-12-27 22:17:13 +0100

    Remove redundant code

--- app/controllers/api/device_type_properties_controller.rb
+++
@@ -1,8 +0,0 @@
-module Api
-  class DeviceTypePropertiesController < ApplicationController
-    def index
-      @dtype_properties = DeviceTypeProperty.all
-      render json: @dtype_properties
-    end
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  root 'devices#index'
-
   namespace :api do
     resources :device_types
     resources :devices
-    resources :device_type_properties, only: [:index]
-    resources :device_property_values, only: %i[index show]
   end
 end
