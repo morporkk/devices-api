0332e5adfab5571451ba9d93975296790e3b850c
Miroslav <miroslavr@protonmail.com>
2019-12-27 01:43:51 +0100

    Prevent destruction of DeviceType with Devices
    Cant destroy DeviceType record if there's any Device record with id of
    given DeviceType

--- app/models/device_type.rb
+++ app/models/device_type.rb
@@ -1,7 +1,9 @@
 class DeviceType < ApplicationRecord
   acts_as_tree
+
   # belongs_to :parent, class_name: :DeviceType, optional: true
   # has_many   :children, class_name: :DeviceType, foreign_key: :parent_id
+  has_many :devices, dependent: :restrict_with_exception
 
   validates :name, presence: true, length: { maximum: 65 },
                    uniqueness: true
